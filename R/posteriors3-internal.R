8b25d9fe44b349098c82b29ddf43a890bf2042b9
Paul Northrop <p.northrop@ucl.ac.uk>
2024-12-06 23:33:26 +0000

    Check the support in check_data()

--- R/posteriors3-internal.R
+++ R/posteriors3-internal.R
@@ -21,11 +21,37 @@ check_data <- function(x) {
     stop(paste0("'data' must have length ", length(x),
                 "create is using add_data()"))
   }
+  # Check that all the data are in the support of the relevant random variable
+  # Find the support of x
+  # This is a length(d) by 2 matrix, with columns min and max
+  the_support <- distributions3::support(x)
+  the_data <- attr(x, "data")
+  mins <- the_support[, "min"]
+  maxs <- the_support[, "max"]
+  check_support <- function(data, mins, maxs) {
+    return(ifelse (all(data >= mins & data <= maxs), TRUE, FALSE))
+  }
+  valid <- mapply(check_support, the_data, mins, maxs)
+  if (any(!valid)) {
+    stop(paste0("The data for variable number(s): ",
+                paste(which(!valid), collapse = ", "),
+                "; are outside their support"))
+  }
+  check_integer <- function(data, discrete) {
+    return(any(!is.wholenumber(data) & discrete))
+  }
+  discrete <- distributions3::is_discrete(x)
+  not_valid <- mapply(check_integer, the_data, discrete)
+  if (any(not_valid)) {
+    stop(paste0("The data for variable number(s): ",
+                paste(which(not_valid), collapse = ", "),
+                "; are not all integers"))
+  }
   return(invisible(x))
 }
 
 #' @keywords internal
 #' @rdname posteriors3-internal
-check_support <- function(x) {
-  return(invisible())
+is.wholenumber <- function(x, tol = .Machine$double.eps ^ 0.5) {
+  return(abs(x - round(x)) < tol)
 }
