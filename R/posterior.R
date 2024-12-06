9b12fd50bc107b935c1b2e30f797b94b470da19a
Paul Northrop <p.northrop@ucl.ac.uk>
2024-12-06 22:16:36 +0000

    Edit examples

--- R/posterior.R
+++ R/posterior.R
@@ -3,14 +3,15 @@
 #' Generic functions for performing Bayesian inference using `"distribution"`
 #' objects. After using [`add_data()`] to add a random sample of data to a
 #' distribution object `x` the generic multiplication `*.distribution`
-#' multiplies the likelihood by the prior distribution specified in `y` to
-#' create a posterior distribution.
+#' multiplies the implied likelihood in `x` by the prior distribution specified
+#' in `y` to create a posterior distribution. `posterior(x, y)` will give the
+#' same output.
 #'
 #' @param x To specify the **likelihood**. A probability distribution object
 #'   inheriting from class `"distribution"` such as those created by a call to
 #'   [`Normal()`][distributions3::Normal()] or
-#'   [`Binomial()`][distributions3::Binomial()] etc. `x` must have a data
-#'   attribute, added using [`add_data()`].
+#'   [`Binomial()`][distributions3::Binomial()] etc. `x` must have an extra
+#'   attribute `"data"`, added using [`add_data()`].
 #' @param y To specify the **prior** distribution. A probability distribution
 #'   object inheriting from class `"distribution"`.
 #'
@@ -28,22 +29,40 @@
 #' @examples
 #' library(distributions3)
 #'
-#' # Note: the value of p is irrelevant
-#' x <- Binomial(size = 10)
-#' x <- add_data(x, 1:3)
-#' # Using a conjugate prior distribution
-#' y <- Beta(alpha = 1, beta = 1)
-#' z <- x * y
-#' z
+#' ## One Binomial distribution, with size 10
+#' ## Note: the argument p is irrelevant
 #'
-#' # Two binomial distributions
-#' x <- Binomial(size = c(5, 10))
-#' # Two vectors of data
+#' distribution <- Binomial(size = 10)
+#'
+#' # Add the data, a numeric vector c(1, 2, 3)
+#' likelihood <- add_data(distribution, 1:3)
+#'
+#' # Set a conjugate prior distribution
+#' prior <- Beta(alpha = 1, beta = 1)
+#' # Determine the posterior distribution
+#' posterior <- likelihood * prior
+#' posterior
+#'
+#' ## Two Binomial distributions, with sizes 5 and 10
+#'
+#' distributions <- Binomial(size = c(5, 10))
+#'
+#' # Below are three equivalent ways to supply the data
+#' #   1,2 for the first Binomial distribution, and
+#' #   1, 2, 3 for the second Binomial distribution
+#'
+#' # data is a list of length 2: one numeric vector per Binomial distribution
 #' data <- list(1:2, 1:3)
-#' x <- add_data(x, data)
-#' y <- Beta(alpha = 2, beta = 2)
-#' z <- x * y
-#' z
+#' # data is a data frame with 2 variables: one per Binomial distribution
+#' data <- data.frame(data1 = c(1:2, NA), data2 = 1:3)
+#' # data is a matrix with 2 columns: one per Binomial distribution
+#' data <- matrix(c(1:2, NA, 1:3), nrow = 3, ncol = 2)
+#'
+#' likelihood <- add_data(distributions, data)
+#' prior <- Beta(alpha = 1.5, beta = 2.1)
+#' posterior <- likelihood * prior
+#' posterior
+#'
 #' @export
 #' @name posterior
 NULL
@@ -55,11 +74,7 @@ NULL
 `*.distribution` <- function(x, y) {
   # Check that x and y are distribution objects
   if (!inherits(x, "distribution") | !inherits(y, "distribution")) {
-    stop("x and y must each be a \"distribution\" object")
-  }
-  # Check that the likelihood object x has data as an attribute
-  if (is.null(attr(x, "data"))) {
-    stop("x must have a data attribute, added using add_data()")
+    stop("'x' and 'y' must each be a \"distribution\" object")
   }
   # Call the relevant S3 posterior method based on data distribution, that is,
   # class(x)[1], passing the prior distribution y
