22e530a22992eef0212dd71fc232067bb075946f
Paul Northrop <p.northrop@ucl.ac.uk>
2024-12-07 09:55:30 +0000

    Debug

--- R/posteriors3-internal.R
+++ R/posteriors3-internal.R
@@ -14,24 +14,31 @@ check_data <- function(x) {
   if (is.null(attr(x, "data"))) {
     stop("'x' must have a data attribute, added using add_data()")
   }
+  # Extract the data
+  data <- attr(x, "data")
   if (!inherits(data, "list")) {
     stop("The attribute 'data' must be a list, created by add_data()")
   }
-  if (length(attr(x, "data")) != length(x)) {
+  if (length(data) != length(x)) {
     stop(paste0("'data' must have length ", length(x),
                 "create is using add_data()"))
   }
   # Check that all the data are in the support of the relevant random variable
   # Find the support of x
-  # This is a length(d) by 2 matrix, with columns min and max
+  # If length(x) > 1 this is a length(d) by 2 matrix, with columns min and max
+  # If length(x) = 1 this is a numeric vector of length 2
   the_support <- distributions3::support(x)
-  the_data <- attr(x, "data")
-  mins <- the_support[, "min"]
-  maxs <- the_support[, "max"]
-  check_support <- function(data, mins, maxs) {
-    return(ifelse (all(data >= mins & data <= maxs), TRUE, FALSE))
+  if (length(x) > 1) {
+    mins <- the_support[, "min"]
+    maxs <- the_support[, "max"]
+  } else {
+    mins <- the_support["min"]
+    maxs <- the_support["max"]
   }
-  valid <- mapply(check_support, the_data, mins, maxs)
+  check_support <- function(data, mins, maxs) {
+    return(ifelse(all(data >= mins & data <= maxs), TRUE, FALSE))
+  }
+  valid <- mapply(check_support, data, mins, maxs)
   if (any(!valid)) {
     stop(paste0("The data for variable number(s): ",
                 paste(which(!valid), collapse = ", "),
@@ -41,7 +48,7 @@ check_data <- function(x) {
     return(any(!is.wholenumber(data) & discrete))
   }
   discrete <- distributions3::is_discrete(x)
-  not_valid <- mapply(check_integer, the_data, discrete)
+  not_valid <- mapply(check_integer, data, discrete)
   if (any(not_valid)) {
     stop(paste0("The data for variable number(s): ",
                 paste(which(not_valid), collapse = ", "),
