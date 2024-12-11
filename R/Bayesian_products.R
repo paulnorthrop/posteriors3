10e9c00b9ee1696f4350649b3831a252b2b6b1d4
Paul Northrop <p.northrop@ucl.ac.uk>
2024-12-11 23:52:25 +0000

    Add ()

--- R/Bayesian_products.R
+++ R/Bayesian_products.R
@@ -44,7 +44,7 @@
 #'   distribution used.
 #'
 #' @seealso [`add_data()`] for adding data to a `"distribution"` object.
-#' @seealso [`plot.posterior`] for plotting posterior and prior distributions.
+#' @seealso [`plot.posterior()`] for plotting posterior and prior distributions.
 #'
 #' @examples
 #' library(distributions3)
