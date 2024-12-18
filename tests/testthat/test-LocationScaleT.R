e9e5bfe105892b810e3cc9124e64de48504f1b0a
Paul Northrop <p.northrop@ucl.ac.uk>
2024-12-18 00:06:40 +0000

    Test Location-Scale t disribution

---
+++ tests/testthat/test-LocationScaleT.R
@@ -0,0 +1,63 @@
+# Check that the methods for the "LocationScaleT" class are consistent with
+# those for the "StudentsT" class in the distributions3 package
+
+library(distributions3)
+mu <- 10
+sigma <- 2
+X <- LocationScaleT(mu = mu, sigma = sigma, df = 6)
+Y <- StudentsT(6)
+
+# Moments
+test_that("LocationScaleT vs StudentsT: mean", {
+  testthat::expect_equal(mean(X) - mu, mean(Y))
+})
+test_that("LocationScaleT vs StudentsT: variance", {
+  testthat::expect_equal(variance(X) / sigma ^ 2, variance(Y))
+})
+test_that("LocationScaleT vs StudentsT: skewness", {
+  testthat::expect_equal(skewness(X), skewness(Y))
+})
+test_that("LocationScaleT vs StudentsT: kurtosis", {
+  testthat::expect_equal(kurtosis(X), kurtosis(Y))
+})
+
+# pdf, log_pdf, cdf, quantile
+
+xvals <- c(-2:2)
+pvals <- c(0.05, 0.25, 0.5, 0.75, 0.95)
+test_that("LocationScaleT vs StudentsT: pdf", {
+  testthat::expect_equal(pdf(X, xvals), pdf(Y, (xvals - mu) / sigma) / sigma)
+})
+test_that("LocationScaleT vs StudentsT: log_pdf", {
+  testthat::expect_equal(log_pdf(X, xvals),
+                         log_pdf(Y, (xvals - mu) / sigma) - log(sigma))
+})
+test_that("LocationScaleT vs StudentsT: cdf", {
+  testthat::expect_equal(cdf(X, xvals), cdf(Y, (xvals - mu) / sigma))
+})
+test_that("LocationScaleT vs StudentsT: quantile", {
+  testthat::expect_equal(cdf(X, xvals), cdf(Y, (xvals - mu) / sigma))
+})
+test_that("LocationScaleT vs StudentsT: quantile", {
+  testthat::expect_equal(quantile(X, pvals), sigma * quantile(Y, pvals) + mu)
+})
+# random
+set.seed(42)
+randomX <- random(X, 5)
+set.seed(42)
+randomY <- sigma * random(Y, 5) + mu
+test_that("LocationScaleT vs StudentsT: random", {
+  testthat::expect_equal(randomX, randomY)
+})
+
+# support, is_continuous, is_discrete
+test_that("LocationScaleT vs StudentsT: support", {
+  testthat::expect_equal(support(X), c(min = -Inf, max = Inf))
+})
+test_that("LocationScaleT vs StudentsT: is_continuous", {
+  testthat::expect_equal(is_continuous(X), TRUE)
+})
+test_that("LocationScaleT vs StudentsT: support", {
+  testthat::expect_equal(is_discrete(X), FALSE)
+})
+
