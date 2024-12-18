443113e624bb4fd6756f40ce06fe2a00424d3ccb
Paul Northrop <p.northrop@ucl.ac.uk>
2024-12-18 00:43:01 +0000

    Normal-Gamma posterior

--- R/Normal.R
+++ R/Normal.R
@@ -3,32 +3,34 @@
 posterior.Normal <- function(x, y) {
   # Extract the name of the prior distribution
   prior_distribution <- class(y)[1]
-  # If prior is conjugate then infer the posterior distribution
+  # If the prior is a conjugate Normal-Gamma prior for (mu, tau), where mu is
+  # the Normal mean and tau = 1 / sigma ^ 2 = 1 / variance is the precision
+  # then infer the posterior distribution
   if (prior_distribution == "NormalGamma") {
     # Extract the parameter values of the Normal prior for mu
-    mu0 <- y[[1]]$mu
-    nu <- y[[1]]$sigma ^ 2
-    # Extract the parameter values of the gamma prior for tau = 1 / sigma ^ 2
-    alpha <- y[[2]]$shape
-    beta <- y[[2]]$rate
+    # We use the parameterisation in the continuous table on the Wikipedia page
+    # https://en.wikipedia.org/wiki/Conjugate_prior, that is,
+    # (mu, lambda, shape, rate) = (mu0, nu, alpha, beta)
+    mu0 <- y$mu
+    nu <- y$lambda
+    alpha <- y$shape
+    beta <- y$rate
     # Extract the data
-    # Use unlist() to force data to be a vector
+    # Use unlist() to force the data to be a vector
     data <- unlist(attr(x, "data"))
     # Sample size
     n <- length(data)
+    # Calculate the parameters of the Normal-Gamma posterior for (mu, tau)
     # Find the normal posterior for mu
-    mu_mean <- (nu * mu0 + n * mean(data)) / (nu + n)
-    mu_sd <- 1 / sqrt(nu + n)
-    posterior_mu <- distributions3::Normal(mu = mu_mean, sigma = mu_sd)
-    # Find the gamma posterior for tau = 1 / sigma ^ 2
-    tau_alpha <- alpha + n / 2
-    tau_beta <- beta + (n - 1) * stats::var(data) / 2 +
+    posterior_mu <- (nu * mu0 + n * mean(data)) / (nu + n)
+    posterior_lambda <- nu + n
+    posterior_shape <- alpha + n / 2
+    posterior_rate <- beta + (n - 1) * stats::var(data) / 2 +
       (n * nu) * (mean(x) - mu0) ^ 2 / (2 * (nu + n))
-    posterior_tau <- distributions3::Gamma(shape = tau_alpha, rate = tau_beta)
     # Return the posterior as a "distribution" object
-    z <- posterior_mu * posterior_tau
-    # Adjust the class, by removing the second component ("prior")
-    class(z) <- class(z)[-2]
+    z <- NormalGamma(mu = posterior_mu, lambda = posterior_lambda,
+                     shape = posterior_shape, rate = posterior_rate,
+                     names = attr(y, "variable_names"))
   } else {
     stop("A conjugate prior must be used for Normal data")
   }
@@ -36,7 +38,7 @@ posterior.Normal <- function(x, y) {
   attr(z, "prior") <- y
   # Add the likelihood as an attribute for later use
   attr(z, "likelihood") <- x
-  # Store the name of the parameter of interest
+  # Store the names of the parameters of interest
   attr(z, "parameter") <- c("mu", "tau")
   # Add "posterior" as the second component of the class
   class(z) <- c(class(z)[1], "posterior", class(z)[2])
