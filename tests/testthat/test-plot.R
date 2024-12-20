f02428d2048c4041b71b5d327a42b63b9d84a7d6
Paul Northrop <p.northrop@ucl.ac.uk>
2024-12-20 23:00:50 +0000

    Add Normal posterior plot tests

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -15,9 +15,37 @@ beta <- 1
 prior <- Beta(alpha = alpha, beta = beta)
 # Construct the posterior distribution
 posterior <- likelihood * prior
-z <- plot(posterior)
 
 # posterior has extra attributes
+z <- plot(posterior)
 test_that("posterior: 1 Binomial distribution, 1 prior", {
   testthat::expect_equal(attr(posterior, "prior"), z[2], ignore_attr = TRUE)
+})
+
+# posterior has extra attributes
+z <- plot(posterior, prior = FALSE)
+test_that("posterior: 1 Binomial distribution, 1 prior, no prior in plot", {
+  testthat::expect_equal(posterior, z[1], ignore_attr = TRUE)
+})
+
+# One Normal distribution, 1 prior
+
+X <- Normal(mu = 10, sigma = 2)
+
+# Simulate a sample of size 15 from a Normal(10, 2^2) distribution
+set.seed(3)
+data <- random(X, 15)
+# Add the data, a numeric vector
+likelihood <- add_data(X, data)
+
+# Set a Normal-Gamma prior for (mean mu, precision tau)
+prior <- NormalGamma(names = c("mu", "tau"))
+posterior <- likelihood * prior
+posterior
+
+# Plot the marginal prior and posterior distributions for mu and tau
+z <- plot(posterior, margin = "mu")
+margins <- marginals(posterior)
+test_that("posterior: 1 Normal distribution, 1 prior", {
+  testthat::expect_equal(margins[[1]], z[1], ignore_attr = TRUE)
 })
