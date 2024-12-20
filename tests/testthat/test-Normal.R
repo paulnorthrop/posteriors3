47e8bb2c50fbf6e94b289ca7d3cd48e379813c5e
Paul Northrop <p.northrop@ucl.ac.uk>
2024-12-20 22:51:23 +0000

    Add another Normal test

--- tests/testthat/test-Normal.R
+++ tests/testthat/test-Normal.R
@@ -28,6 +28,19 @@ likelihood <- add_data(X, data)
 posterior <- likelihood * prior
 correct_posterior <- NormalGamma(mu = mean(data), lambda = lambda + 2,
                                  shape = shape + 1, rate = rate)
-test_that("posterior: 1 Normal distribution, n = 1", {
+test_that("posterior: 1 Normal distribution, n = 2", {
+  testthat::expect_equal(posterior, correct_posterior, ignore_attr = TRUE)
+})
+
+# Special case: n = 3, x = c(mu0 - 1, mu0, mu0 + 1)
+
+prior <- NormalGamma(mu = mu, lambda = lambda, shape = shape, rate = rate,
+                     names = c("mu", "tau"))
+data <- c(mu - 1, mu, mu + 1)
+likelihood <- add_data(X, data)
+posterior <- likelihood * prior
+correct_posterior <- NormalGamma(mu = mean(data), lambda = lambda + 3,
+                                 shape = shape + 3 / 2, rate = rate + 1)
+test_that("posterior: 1 Normal distribution, n = 3", {
   testthat::expect_equal(posterior, correct_posterior, ignore_attr = TRUE)
 })
