7129068b29ee84e41584045e73c65dff93874a58
Paul Northrop <p.northrop@ucl.ac.uk>
2024-12-23 23:15:06 +0000

    Move calculations with data and add likelihood as a NormalGamma distribution object

--- R/Normal.R
+++ R/Normal.R
@@ -6,6 +6,15 @@ posterior.Normal <- function(x, y) {
   # If the prior is a conjugate Normal-Gamma prior for (mu, tau), where mu is
   # the Normal mean and tau = 1 / sigma ^ 2 = 1 / variance is the precision
   # then infer the posterior distribution
+  # Extract the data
+  # Use unlist() to force the data to be a vector
+  data <- unlist(attr(x, "data"))
+  # Sample size
+  n <- length(data)
+  # Calculate the parameters of the Normal-Gamma posterior for (mu, tau)
+  # Find the normal posterior for mu
+  sample_mean <- mean(data)
+  sample_variance <- ifelse(n == 1, 0, stats::var(data))
   if (prior_distribution == "NormalGamma") {
     # Extract the parameter values of the Normal prior for mu
     # We use the parameterisation in the continuous table on the Wikipedia page
@@ -15,18 +24,11 @@ posterior.Normal <- function(x, y) {
     nu <- y$lambda
     alpha <- y$shape
     beta <- y$rate
-    # Extract the data
-    # Use unlist() to force the data to be a vector
-    data <- unlist(attr(x, "data"))
-    # Sample size
-    n <- length(data)
-    # Calculate the parameters of the Normal-Gamma posterior for (mu, tau)
-    # Find the normal posterior for mu
     posterior_mu <- (nu * mu0 + n * mean(data)) / (nu + n)
     posterior_lambda <- nu + n
     posterior_shape <- alpha + n / 2
-    posterior_rate <- beta + (n - 1) * ifelse(n == 1, 0, stats::var(data)) / 2 +
-      (n * nu) * (mean(data) - mu0) ^ 2 / (2 * (nu + n))
+    posterior_rate <- beta + (n - 1) * sample_variance / 2 +
+      (n * nu) * (sample_mean - mu0) ^ 2 / (2 * (nu + n))
     # Return the posterior as a "distribution" object
     z <- NormalGamma(mu = posterior_mu, lambda = posterior_lambda,
                      shape = posterior_shape, rate = posterior_rate,
@@ -46,7 +48,9 @@ posterior.Normal <- function(x, y) {
   # Add the prior(s) as an attribute for later use
   attr(z, "prior") <- y
   # Add the likelihood as an attribute for later use
-  attr(z, "likelihood") <- x
+  attr(z, "likelihood") <- NormalGamma(mu = sample_mean, lambda = n,
+                                       shape = (n + 1) / 2,
+                                       rate = (n - 1) * sample_variance / 2)
   # Add "posterior" as the second component of the class
   class(z) <- c(class(z)[1], "posterior", class(z)[2])
   return(z)
