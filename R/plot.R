bb0b6f21116b1703b97936a7653bf1c60a183ca0
Paul Northrop <p.northrop@ucl.ac.uk>
2024-12-23 00:10:38 +0000

    Add 2D posterior and prior contour plots

--- R/plot.R
+++ R/plot.R
@@ -1,8 +1,9 @@
 #' Plot posterior and prior distributions
 #'
 #' Plot method for an object inheriting from class `"posterior"`.
-#' Plots the posterior and corresponding prior density functions.
-#' The cumulative distribution functions will be plotted if `cdf = TRUE`.
+#' Plots posterior and corresponding prior density functions.
+#' Supports one-dimensional distributions, including one-dimensional marginal
+#' distributions and two-dimensional distributions.
 #'
 #' @param x an object of class `c("name", "distribution")`, where
 #'   `"name"` is the name of the distribution.
@@ -12,17 +13,25 @@
 #' @param margin A numeric or character scalar giving the number or the name
 #'   of the marginal variable. `attr(x, "variable_names")` can be used to view
 #'   the names of the variables.
-#' @param cdf A logical scalar.  If `cdf = TRUE` then the cumulative
-#'   distribution function (c.d.f.) is plotted.  Otherwise, the probability
-#'   density function (p.d.f.), for a continuous variable, or the probability
-#'   mass function (p.m.f.), for a discrete variable, is plotted.
-#' @param p A numeric vector.  If `xlim` is not passed in `...`
-#'   then `p` is the fallback option for setting the range of values
-#'   over which the p.m.f, p.d.f. or c.d.f is plotted.  See **Details**.
-#' @param len An integer scalar.  If `x` is a continuous distribution
-#'   object then `len` is the number of values at which the p.d.f or
-#'   c.d.f. is evaluated to produce the plot.  The larger `len` is the
-#'   smoother is the curve.
+#' @param cdf A logical scalar.  In one-dimensional cases, if `cdf = TRUE` then
+#'   the cumulative distribution function (c.d.f.) is plotted.  Otherwise, the
+#'   probability density function (p.d.f.) is plotted. Not relevant for
+#'   two-dimensional cases.
+#' @param p A numeric vector.  In one-dimensional cases, if `xlim` is not
+#'   passed in `...` then `p` is the fallback option for setting the range of
+#'   values over which the p.d.f. or c.d.f is plotted.  See **Details**.
+#'   In two-dimensional cases, `p` provides the levels of probability contours.
+#'   The respective probabilities that the variable in question lies within the
+#'   contour are approximately equal to the vales in `p`.
+#'   The default `p = c(5, 25, 50, 75, 95)` is set internally.
+#'   If `xlim` and `ylim` are passed in `...` then these are used to set the
+#'   area over which the p.d.f. is contoured.  Otherwise, this is based on the
+#'   smallest and largest components of `p`.
+#' @param len An integer scalar.  In one-dimensional cases, the number of
+#'   values at which the p.d.f or c.d.f. is evaluated to produce the plot.
+#'   The larger `len` is the smoother is the curve. In two-dimensional cases,
+#'   a `len` nu `len` regular grid is used to produce probability contours,
+#'   using [`contour()`][graphics::contour].
 #' @param legend_args A list of arguments to be passed to
 #'   [`legend()`][graphics::legend()].  In particular, the argument `x`
 #'   (perhaps in conjunction with `legend_args$y`) can be used to set the
@@ -32,13 +41,13 @@
 #' @param digits The number of significant digits to be used in values of
 #'   parameters displayed in the title or legend.
 #'   See [`signif()`][base::signif].
-#' @param ...  Further arguments to be passed to [`plot()`][graphics::plot()],
-#'   [`ecdf()`][stats::ecdf] and [`lines()`][graphics::lines()],
-#'   such as `xlim`, `ylim`, `xlab`, `ylab`, `main`, `lwd`, `lty`, `col`, `pch`.
-#' @details If `xlim` is passed in `...` then this determines the
-#'   range of values of the variable to be plotted on the horizontal axis.
-#'   If `xlim` is not passed in `...` then the range of values spans
-#'   the support of the distribution, with the following proviso: if the
+#' @param ...  Further arguments to be passed to [`plot()`][graphics::plot()]
+#'   and [`contour()`][graphics::contour()] such as `xlim`, `ylim`, `xlab`,
+#'   `ylab`, `main`, `lwd`, `lty`, `col`, `pch`.
+#' @details In one-dimensional cases, if `xlim` is passed in `...` then this
+#'   determines the range of values of the variable to be plotted on the
+#'   horizontal axis. If `xlim` is not passed in `...` then the range of values
+#'   spans the support of the distribution, with the following proviso: if the
 #'   lower (upper) endpoint of the distribution is `-Inf` (`Inf`)
 #'   then the lower (upper) limit of the plotting range is set to the
 #'   `p[1]`percent (`p[2]`percent) quantile of the distribution.
@@ -53,9 +62,9 @@
 #' @section Examples:
 #' See the examples in [`posterior`].
 #' @export
-plot.posterior <- function(x, prior = TRUE, margin = NULL, cdf = FALSE,
-                           p = c(0.1, 99.9), len = 1000, legend_args = list(),
-                           digits = 3, ...) {
+plot.posterior <- function(x, prior = TRUE, likelihood = FALSE, margin = NULL,
+                           cdf = FALSE, p = c(0.1, 99.9), len = 1000,
+                           legend_args = list(), digits = 3, ...) {
   if (!distributions3::is_distribution(x)) {
     stop("use only with \"distribution\" objects")
   }
@@ -108,6 +117,22 @@ plot.posterior <- function(x, prior = TRUE, margin = NULL, cdf = FALSE,
     }
     variable_name <- attr(x, "variable_names")
   }
+  #
+  if (number_of_margins > 2 & is.null(margin)) {
+    stop("Only 1D and 2D distributions can be plotted")
+  } else if (number_of_margins == 2 & is.null(margin)) {
+    if (missing(len)) {
+      len <- 101
+    }
+    if (missing(p)) {
+      p <- c(5, 25, 50, 75, 95)
+    }
+    plot_distribution_contours(x, prior = prior,
+                               names = marginal_variable_names, len = len,
+                               p = p, legend_args = legend_args,
+                               digits = digits, ...)
+    return(invisible(x))
+  }
   # Extract the name of the distribution
   distn <- class(x)[1]
   # The number of parameters and their names
@@ -136,10 +161,18 @@ plot.posterior <- function(x, prior = TRUE, margin = NULL, cdf = FALSE,
     }
     my_main <- paste0(my_main, signif(as.matrix(x)[np], digits = digits), ")")
   }
-  if (cdf) {
-    my_main <- paste(my_main, "c.d.f.")
+  if (prior) {
+    if (cdf) {
+      my_main <- paste(my_main, "c.d.f.")
+    } else {
+      my_main <- paste(my_main, "p.d.f.")
+    }
   } else {
-    my_main <- paste(my_main, "p.d.f.")
+    if (cdf) {
+      my_main <- paste(my_main, "posterior c.d.f.")
+    } else {
+      my_main <- paste(my_main, "posterior p.d.f.")
+    }
   }
   # Extract user-supplied arguments for graphics::plot()
   user_args <- list(...)
@@ -162,7 +195,6 @@ plot.posterior <- function(x, prior = TRUE, margin = NULL, cdf = FALSE,
   }
   # Set x and y axis labels, based on the name of the parameter
   # We include the discrete case, but this probably will not be used
-#  variable_name <- attr(x, "variable_names")
   my_xlab <- variable_name
   if (cdf) {
     my_ylab <- paste0("F(", my_xlab, ")")
@@ -206,7 +238,7 @@ plot.posterior <- function(x, prior = TRUE, margin = NULL, cdf = FALSE,
         if (prior) {
           legend_args$title <- paste("posterior", "prior", sep = "        ")
         } else {
-          legend_args$title <- "posterior"
+          legend_args$title <- NULL
         }
       }
       if (is.null(legend_args[["col"]])) {
@@ -227,8 +259,9 @@ plot.posterior <- function(x, prior = TRUE, margin = NULL, cdf = FALSE,
       }
       do.call(graphics::legend, legend_args)
     }
+    return(invisible())
   }
-  # Start the legend. If legend$x hasn't been supplied then set defaults.
+  # Start the legend. If legend$x hasn't been supplied then set defaults
   if (is.null(legend_args[["x"]])) {
     if (cdf) {
       legend_args[["x"]] <- "bottomright"
@@ -241,7 +274,7 @@ plot.posterior <- function(x, prior = TRUE, margin = NULL, cdf = FALSE,
   if (prior) {
     my_lty <- rep(1:2, each = n_posteriors)
   } else {
-    my_lty = 1
+    my_lty <- 1
   }
   continuous_plot(x, xvals, ...)
   return(invisible(x))
--- R/posteriors3-internal.R
+++ R/posteriors3-internal.R
@@ -69,3 +69,188 @@ check_data <- function(x) {
 is.wholenumber <- function(x, tol = .Machine$double.eps ^ 0.5) {
   return(abs(x - round(x)) < tol)
 }
+
+# Need to pick a (prior, posterior) combination
+
+#' @keywords internal
+#' @rdname posteriors3-internal
+plot_distribution_contours <- function(x, prior, names, len, p, legend_args,
+                                       digits, ...) {
+  # Extract the the arguments passed in ...
+  dots_args <- list(...)
+  # Note: the prior distributions have already been added to x
+  # Extract the marginal distributions
+  posterior_variable <- paste0("posterior for ", names)
+  prior_variable <- paste0("prior for ", names)
+  variable_name <- names
+  # Find the number of posteriors (and priors)
+  n_posteriors <- length(attr(x, posterior_variable[[1]])[[1]])
+  # The total number of distributions (posteriors and priors)
+  n_distns <- length(x)
+  # Function to find the extreme quantiles of the marginals
+  quantile_function <- function(i, j, variable, probs, prior) {
+    if (prior) {
+      y <- attr(x, prior_variable[[j]])[[1]][i]
+    } else {
+      y <- attr(x, posterior_variable[[j]])[[1]][i]
+    }
+    return(quantile(y, probs = probs))
+  }
+  # Convert percentages to probabilities
+  prob <- p / 100
+  # Set the default posterior and prior ranges
+  prob_range <- range(prob)
+  # Extend the extreme probabilities a little to try to include all contours
+  prob_range[1] <- prob_range[1] / 10
+  prob_range[2] <- 1 - (1 - prob_range[2]) / 10
+  # Set the area over which the pdf is contoured using xlim and/or ylim, if
+  # these are supplied in ..., or the default based on prob
+  if (!is.null(dots_args$xlim)) {
+    x_range <- dots_args$xlim
+  } else {
+    x_range <- range(sapply(1:n_posteriors, quantile_function, j = 1,
+                            probs = prob_range, prior = FALSE))
+    if (prior) {
+      x_prior_range <- range(sapply(1:n_posteriors, quantile_function, j = 1,
+                                    probs = prob_range, prior = TRUE))
+      x_range <- range(x_range, x_prior_range)
+    }
+  }
+  if (!is.null(dots_args$ylim)) {
+    y_range <- dots_args$ylim
+  } else {
+    y_range <- range(sapply(1:n_posteriors, quantile_function, j = 2,
+                            probs = prob_range, prior = FALSE))
+    if (prior) {
+      y_prior_range <- range(sapply(1:n_posteriors, quantile_function, j = 2,
+                                    probs = prob_range, prior = TRUE))
+      y_range <- range(y_range, y_prior_range)
+    }
+  }
+  xx <- seq(x_range[1], x_range[2], length.out = 101)
+  yy <- seq(y_range[1], y_range[2], length.out = 101)
+  xxyy <- as.matrix(expand.grid(xx, yy))
+  # Extract the name of the distribution
+  distn <- class(x)[1]
+  # The number of parameters and their names
+  np <- length(colnames(x))
+  par_names <- colnames(x)
+  # Create a title for the plot
+  # If n_distns = 1 then place the parameter values in the title
+  # If n_distns > 1 then place the parameter values in the legend
+  if (n_distns > 1) {
+    my_main <- paste0(distn, " (")
+    if (np > 1) {
+      for (i in 1:(np - 1)) {
+        my_main <- paste0(my_main, par_names[i], ", ")
+      }
+    }
+    my_main <- paste0(my_main, par_names[np], ")")
+  } else {
+    my_main <- paste0(distn, " (")
+    # Round the parameter values to digits significant digits
+    if (np > 1) {
+      for (i in 1:(np - 1)) {
+        my_main <- paste0(my_main, signif(as.matrix(x)[i], digits = digits),
+                          ", ")
+      }
+    }
+    my_main <- paste0(my_main, signif(as.matrix(x)[np], digits = digits), ")")
+  }
+  if (prior) {
+    my_main <- paste(my_main, "p.d.f.")
+  } else {
+    my_main <- paste(my_main, "posterior p.d.f.")
+  }
+  # Function to create the legend text
+  create_legend_text <- function(x, n_distns) {
+    leg_text <- numeric(n_distns)
+    for (i in 1:n_distns) {
+      text_i <- lapply(x, "[[", i)
+      # Round the parameter values to digits significant digits
+      text_i <- lapply(text_i, signif, digits = digits)
+      leg_text[i] <- paste0(text_i, collapse = ", ")
+    }
+    return(leg_text)
+  }
+  # Create the contour plot of posterior and prior densities
+  # Function for calculating the pdf
+  pdf_fun <- function(xx, yy, distn_object) {
+    pdf(distn_object, matrix(c(xx, yy), ncol = 2))
+  }
+  # Create a general contour_plot function
+  plot_contour <- function(which_distn, add, lty, col, distn_objects, ...,
+                           xlab = my_xlab, ylab = my_ylab, main = my_main,
+                           lwd = my_lwd) {
+    zz <- outer(xx, yy, FUN = pdf_fun,
+                distn_object = distn_objects[which_distn])
+    dx <- diff(xx[1:2])
+    dy <- diff(yy[1:2])
+    sz <- sort(zz)
+    c1 <- cumsum(sz) * dx * dy
+    c1 <- c1 / max(c1)
+    con_levs <- suppressWarnings(sapply(prob, function(x)
+      stats::approx(c1, sz, xout = 1 - x)$y))
+    graphics::contour(xx, yy, zz, levels = con_levs, add = add, ann = FALSE,
+                      labels = prob * 100, xlab = xlab, ylab = ylab,
+                      main = main, lwd = lwd, lty = lty, col = col, ...)
+    return(invisible())
+  }
+  # Vectors for adding to first contour plot and line types and colours
+  add_vec <- c(FALSE, rep(TRUE, n_distns - 1))
+  if (prior) {
+    lty_vec <- rep(1:2, each = n_posteriors)
+  } else {
+    lty_vec <- 1
+  }
+  col_vec <- 1:n_posteriors
+  # Default graphics parameters
+  my_xlab <- names[1]
+  my_ylab <- names[2]
+  my_lwd <- 2
+  # Allow user-override of lty and col
+  save_dots_args <- dots_args
+  if (!is.null(dots_args$lty)) {
+    lty_vec <- dots_args$lty
+    save_dots_args["lty"] <- NULL
+  }
+  if (!is.null(dots_args$col)) {
+    col_vec <- dots_args$col
+    save_dots_args["col"] <- NULL
+  }
+  if (!is.null(dots_args$lwd)) {
+    lwd_vec <- dots_args$lwd
+  }
+  MoreArgs <- c(list(distn_objects = x), save_dots_args)
+  mapply(FUN = plot_contour, which_distn = 1:n_distns, add = add_vec,
+         lty = lty_vec, col = col_vec, MoreArgs = MoreArgs)
+  # If n_distns > 1 then add a legend
+  if (n_distns > 1) {
+    legend_args$col <- col_vec
+    legend_args$lwd <- lwd_vec
+    legend_args$lty <- lty_vec
+    if (is.null(legend_args[["legend"]])) {
+      legend_args$legend <- create_legend_text(x, n_distns)
+    }
+    if (is.null(legend_args[["title"]])) {
+      if (prior) {
+        legend_args$title <- paste("posterior", "prior", sep = "        ")
+      } else {
+        legend_args$title <- NULL
+      }
+    }
+    if (is.null(legend_args[["ncol"]])) {
+      if (prior) {
+        legend_args$ncol <- 2
+      } else {
+        legend_args$ncol <- 1
+      }
+    }
+    # If legend$x hasn't been supplied then set defaults
+    if (is.null(legend_args[["x"]])) {
+      legend_args[["x"]] <- "topright"
+    }
+    do.call(graphics::legend, legend_args)
+  }
+  return(invisible())
+}
