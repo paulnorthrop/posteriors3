d86dde81aaa62403e2912d7e113ed17cb7c4929e
Paul Northrop <p.northrop@ucl.ac.uk>
2025-01-03 00:11:23 +0000

    Non-conjugate Binomial cases

--- R/Binomial.R
+++ R/Binomial.R
@@ -1,6 +1,6 @@
 #' @export
 #' @rdname posterior
-posterior.Binomial <- function(x, y) {
+posterior.Binomial <- function(x, y, ...) {
   # Extract the name of the prior distribution
   prior_distribution <- class(y)[1]
   # Extract the data and the Binomial size parameter
@@ -21,8 +21,68 @@ posterior.Binomial <- function(x, y) {
     posterior_beta = prior_beta + sum_size_minus_data
     # Return the Beta posterior as a "distribution" object
     z <- Beta(alpha = posterior_alpha, beta = posterior_beta)
+    # Add "posterior" as the second component of the class
+    class(z) <- c(class(z)[1], "posterior", class(z)[2])
   } else {
-    stop("A conjugate Beta prior must be used for Binomial data")
+    # Find the mle to use as an initial estimate
+    mle <- sum_data / (size * length(unlist(data)))
+    # Avoid numerical problems in the cases where the mle is 0 or 1
+    if (mle == 0) {
+      mle <- 0.01
+    } else if (mle == 1) {
+      mle <- 0.99
+    }
+    # Create a function to calculate the log-posterior
+    log_posterior_fn <- function(prob) {
+      if (any(prob < 0) || any(prob > 1)) {
+        return(-Inf)
+      }
+      # Create a Binomial distribution object
+      d <- distributions3::Binomial(size = size, p = prob)
+      log_lik <- log_likelihood(d = d, x = unlist(data))
+      log_prior <- log_pdf(prior, x = prob)
+      return(log_lik + log_prior)
+    }
+    # Has the user provided arguments in ... to be passed to rust::ru()?
+    for_ru <- list(...)
+    # If the simulation size n has not been supplied then set it to 1000
+    if (is.null(for_ru$n)) {
+      for_ru$n <- 1000
+    }
+    # If the transformation argument trans has not been supplied then use
+    # trans = "user" to use a logit transformation on prob
+    # If trans = "BC" has been supplied then call rust::find_lambda_one_d() to
+    # set a suitable value of the Box-Cox parameter lambda
+    # If trans = "none" then use no transformation, but do set the lower and
+    # upper bounds of 0 and 1 for prob
+    if (is.null(for_ru$trans)) {
+      phi_to_theta <- function(phi) {
+        ephi <- exp(phi)
+        return(ephi / (1 + ephi))
+      }
+      log_j <- function(theta) {
+        return(-log(theta) - log(1 - theta))
+      }
+      trans_list <- list(phi_to_theta = phi_to_theta, log_j = log_j)
+      phi_init <- log(mle / (1 - mle))
+      # Set for_ru$trans to NULL in case the user supplied trans = NULL
+      for_ru$trans <- NULL
+      for_ru <- c(for_ru, list(init = phi_init, trans = "user"), trans_list)
+    } else if (for_ru$trans == "BC") {
+      lambda <- rust::find_lambda_one_d(logf = log_posterior_fn,
+                                        min_phi = 0.01, max_phi = 0.99)
+      for_ru <- c(for_ru, list(lambda = lambda, lower = 0, upper = 1))
+    } else if (for_ru$trans == "none") {
+      for_ru <- c(for_ru, list(init = mle, lower = 0, upper = 1))
+    } else {
+      stop("''trans'' must be NULL, \"BC\" or \"none\"")
+    }
+    # Add the log_posterior_fn to the list of argument for rust::ru()
+    for_ru <- c(list(logf = log_posterior_fn), for_ru)
+    # Call rust:ru()
+    z <- do.call(rust::ru, for_ru)
+    # Add "posterior" as the second component of the class
+    class(z) <- c(class(z)[1], "posterior")
   }
   # Add the prior(s) as an attribute for later use
   attr(z, "prior") <- y
@@ -30,7 +90,5 @@ posterior.Binomial <- function(x, y) {
   attr(z, "likelihood") <- Beta(alpha = sum_data, beta = sum_size_minus_data)
   # Store the name of the parameter of interest
   attr(z, "variable_names") <- "p"
-  # Add "posterior" as the second component of the class
-  class(z) <- c(class(z)[1], "posterior", class(z)[2])
   return(z)
 }
